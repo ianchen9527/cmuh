353b54b269b02d47fbd5d0ccbf94380e049cb0a8
ianchen9527 <ianchen9527@gmail.com>
2018-07-14 02:00:18 +0800

    Blood record done

---
+++ src/components/container/BloodRecord/components/FormSelectWrapper/index.js
@@ -0,0 +1,7 @@
+import styled from "styled-components"
+
+const FormSelectWrapper = styled.div`
+  width: 120px;
+`
+
+export default FormSelectWrapper
--- src/components/container/BloodRecord/index.js
+++ src/components/container/BloodRecord/index.js
@@ -6,13 +6,22 @@ import {
   onSaveMedicalRecord,
   onFetchMedicalRecord
 } from "../../../actions/medicalRecord"
-import { Form, Header, Dimmer, Loader, Button } from "semantic-ui-react"
+import {
+  Form,
+  Header,
+  Dimmer,
+  Loader,
+  Button,
+  Table,
+  Select
+} from "semantic-ui-react"
 import FormWrapper from "./components/FormWrapper"
 import Label from "../../presentational/Label"
 import TimePicker from "rc-time-picker"
 import "rc-time-picker/assets/index.css"
 import moment from "moment"
 import DatePickerWrapper from "./components/DatePickerWrapper"
+import FormSelectWrapper from "./components/FormSelectWrapper"
 
 class BloodRecord extends Component {
   constructor() {
@@ -129,6 +138,12 @@ class BloodRecord extends Component {
     this.setState({ bloodRecord: bloodRecord })
   }
 
+  handleRecordSelectChange(index, name, event, data) {
+    let bloodRecord = { ...this.state.bloodRecord }
+    bloodRecord.records[index][name] = data.value
+    this.setState({ bloodRecord: bloodRecord })
+  }
+
   handleTimeChange(name, value) {
     if (value) {
       let bloodRecord = { ...this.state.bloodRecord }
@@ -137,9 +152,31 @@ class BloodRecord extends Component {
     }
   }
 
+  handleRecordTimeChange(index, gradeIndex, name, value) {
+    if (value) {
+      let bloodRecord = { ...this.state.bloodRecord }
+      bloodRecord.records[index].grades[gradeIndex][name] = value.format(
+        "HH:mm"
+      )
+      this.setState({ bloodRecord: bloodRecord })
+    }
+  }
+
   handleChange(name, event) {
     let bloodRecord = { ...this.state.bloodRecord }
     bloodRecord[name] = event.target.value
+    this.setState({ bloodRecord: bloodRecord })
+  }
+
+  handleRecordChange(index, name, event) {
+    let bloodRecord = { ...this.state.bloodRecord }
+    bloodRecord.records[index][name] = event.target.value
+    this.setState({ bloodRecord: bloodRecord })
+  }
+
+  handleRecordGradeChange(index, gradeIndex, name, event) {
+    let bloodRecord = { ...this.state.bloodRecord }
+    bloodRecord.records[index].grades[gradeIndex][name] = event.target.value
     this.setState({ bloodRecord: bloodRecord })
   }
 
@@ -152,6 +189,175 @@ class BloodRecord extends Component {
 
   getMoment(time) {
     return time ? moment(time, "HH:mm") : moment()
+  }
+
+  getRecordPeriod(record) {
+    let period = ""
+    switch (record.id) {
+      case "#1":
+        period = "0~15min"
+        break
+      case "#2":
+        period = "16~30min"
+        break
+      case "#3":
+        period = "31~45min"
+        break
+      case "#4":
+        period = "46~60min"
+        break
+      default:
+        break
+    }
+    return period
+  }
+
+  renderRecordGrade(index, gradeIndex) {
+    return (
+      <Table.Cell>
+        <Table basic="very" celled collapsing>
+          <Table.Body>
+            <Table.Row>
+              <Table.Cell width="2">
+                <Header size="tiny">時間</Header>
+              </Table.Cell>
+              <Table.Cell width="5">
+                <TimePicker
+                  fluid
+                  format="HH:mm"
+                  showSecond={false}
+                  onChange={this.handleRecordTimeChange.bind(
+                    this,
+                    index,
+                    gradeIndex,
+                    "time"
+                  )}
+                  value={this.getMoment(
+                    this.state.bloodRecord.records[index].grades[gradeIndex]
+                      .time
+                  )}
+                />
+              </Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell width="2">
+                <Header size="tiny">體溫</Header>
+              </Table.Cell>
+              <Table.Cell width="5">
+                <input
+                  value={
+                    this.state.bloodRecord.records[index].grades[gradeIndex]
+                      .bodyTemperature
+                  }
+                  onChange={this.handleRecordGradeChange.bind(
+                    this,
+                    index,
+                    gradeIndex,
+                    "bodyTemperature"
+                  )}
+                />
+              </Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell width="2">
+                <Header size="tiny">血壓</Header>
+              </Table.Cell>
+              <Table.Cell width="5">
+                <input
+                  value={
+                    this.state.bloodRecord.records[index].grades[gradeIndex]
+                      .bloodPressure
+                  }
+                  onChange={this.handleRecordGradeChange.bind(
+                    this,
+                    index,
+                    gradeIndex,
+                    "bloodPressure"
+                  )}
+                />
+              </Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell width="2">
+                <Header size="tiny">血氧量</Header>
+              </Table.Cell>
+              <Table.Cell width="5">
+                <input
+                  value={
+                    this.state.bloodRecord.records[index].grades[gradeIndex]
+                      .bloodOxygenation
+                  }
+                  onChange={this.handleRecordGradeChange.bind(
+                    this,
+                    index,
+                    gradeIndex,
+                    "bloodOxygenation"
+                  )}
+                />
+              </Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        </Table>
+      </Table.Cell>
+    )
+  }
+
+  renderRecords() {
+    return this.state.bloodRecord.records.map((record, index) => {
+      return (
+        <Table.Row key={index}>
+          <Table.Cell>{record.id}</Table.Cell>
+          <Table.Cell>{this.getRecordPeriod(record)}</Table.Cell>
+          <Table.Cell>
+            <FormSelectWrapper>
+              <Select
+                fluid
+                onChange={this.handleRecordSelectChange.bind(
+                  this,
+                  index,
+                  "status"
+                )}
+                options={this.statusOptions}
+                value={this.state.bloodRecord.records[index].status}
+              />
+            </FormSelectWrapper>
+          </Table.Cell>
+          <Table.Cell>
+            <FormSelectWrapper>
+              <input
+                value={this.state.bloodRecord.records[index].speed}
+                onChange={this.handleRecordChange.bind(this, index, "speed")}
+              />
+            </FormSelectWrapper>
+          </Table.Cell>
+          {this.renderRecordGrade(index, 0)}
+          {this.renderRecordGrade(index, 1)}
+        </Table.Row>
+      )
+    })
+  }
+
+  renderTable() {
+    return (
+      <Table celled>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>採血管編號</Table.HeaderCell>
+            <Table.HeaderCell>區間</Table.HeaderCell>
+            <Table.HeaderCell>狀態</Table.HeaderCell>
+            <Table.HeaderCell>輸注速度(滴/時)</Table.HeaderCell>
+            <Table.HeaderCell>CRS(Grade 1)發生時間與生理紀錄</Table.HeaderCell>
+            <Table.HeaderCell>
+              CRS(Grade >= 2)發生時間與生理紀錄
+            </Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          <Table.Row />
+          {this.renderRecords()}
+        </Table.Body>
+      </Table>
+    )
   }
 
   render() {
@@ -187,6 +393,7 @@ class BloodRecord extends Component {
               />
             </DatePickerWrapper>
           </Form.Field>
+          {this.renderTable()}
           <Header size="medium" color="red">
             {this.props.errorMessage}
           </Header>
